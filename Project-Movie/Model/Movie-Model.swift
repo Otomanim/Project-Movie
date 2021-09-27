cea7edeba2e88265acd13cb0160b674701bb20a1
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2021-09-27 19:35:59 -0300

    Initial
    implemented customCells,  organized in MVVM

---
+++ Project-Movie/Model/Movie-Model.swift
@@ -0,0 +1,8 @@
+//
+//  Movie-Model.swift
+//  Project-Movie
+//
+//  Created by Evandro Rodrigo Minamoto on 27/09/21.
+//
+
+import Foundation
--- Project-Movie/ViewModel/HomeViewController.swift
+++ Project-Movie/ViewModel/HomeViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
